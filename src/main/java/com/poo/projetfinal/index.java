97214d4656c13ee749120e03b9e733aa05960cc9
paulingaillot <paulingaillot@gmail.com>
2022-11-08 18:32:59 -0500

    Background + Cookies

--- src/main/java/com/poo/projetfinal/index.java
+++ src/main/java/com/poo/projetfinal/index.java
@@ -1,5 +1,6 @@
 package com.poo.projetfinal;
 
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +8,36 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class index {
 
+	public Optional<String> readServletCookie(HttpServletRequest request, String name){
+		return Arrays.stream(request.getCookies())
+		  .filter(cookie->name.equals(cookie.getName()))
+		  .map(Cookie::getValue)
+		  .findAny();
+	  }
+
 	@GetMapping("/")
-	public ModelAndView Index() {
+	public ModelAndView Index(HttpServletRequest request) {
 
 		String url = "jdbc:mysql://127.0.0.1:3306/test";
 		String port = "3308";
@@ -37,7 +61,22 @@ public class index {
 
 		var mav = new ModelAndView("index");
 
-		mav.addObject("username", "Noemie");
+		String name = "newbie";
+		if(!readServletCookie(request, "username").isEmpty()) {
+			name = readServletCookie(request, "username").get();
+		}
+
+		SimpleDateFormat s = new SimpleDateFormat("HH");
+   		Date date = new Date();
+    	
+		if(Integer.parseInt(s.format(date)) >= 18) {
+			mav.addObject("background", "p-3 mb-2 bg-dark text-white");
+		}else {
+			mav.addObject("background", "p-3 mb-2 bg-white text-dark");
+		}
+
+		
+		mav.addObject("username", name);
 
 		return mav;
 	}
@@ -49,7 +88,7 @@ public class index {
 	}
 
 	@PostMapping("/SubmitConnexion")
-	public RedirectView SubmitConnexion(String nom, String password) {
+	public RedirectView SubmitConnexion(String nom, String password, HttpServletResponse response) {
 		System.out.println(nom);
 		System.out.println(password);
 		if (nom != null && password != null) {
@@ -69,6 +108,10 @@ public class index {
 				String pwd = result.getString("password");
 				st.close();
 				if(pwd.equals(password)) {
+					// create a cookie
+    				Cookie cookie = new Cookie("username", nom);
+					response.addCookie(cookie);
+					
 					return new RedirectView("/");
 				}else {
 					return new RedirectView("/connexion");
